ec2c1f48335e2664f32a16631796b5d4af3ab536
sanchita-14 <136978898+sanchita-14@users.noreply.github.com>
2023-08-19 20:28:32 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -70,7 +70,7 @@ function isCollision(rect1,rect2){
     return( 
     rect1.left<=rect2.right &&
     rect1.right>=rect2.left &&
-    rect1.top<-rect2.bottom&&
+    rect1.top<=rect2.bottom&&
     rect1.bottom>=rect2.top
     )
 }
